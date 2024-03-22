f42f97d788be50a435917c6cb44a1911a9d31bfd
silo5k3u8t4 <mineminer363@gmail.com>
2024-03-22 15:42:20 +0530

    renamed:    quiz/branch.html -> branch.html 	renamed:    quiz/ct.html -> ct.html 	renamed:    quiz/ct3_suboptions.html -> ct3_suboptions.html 	renamed:    quiz/dbms.html -> dbms.html 	deleted:    quiz.html 	modified:   scrip/hss.js 	renamed:    quiz/style.css -> style.css

--- scrip/hss.js
+++ scrip/hss.js
@@ -8,7 +8,7 @@ const boxes = document.querySelectorAll('.box');
             }
             if(content=="Quiz Time")
             {
-            	window.location.href = "/quiz/branch.html";
+            	window.location.href = "branch.html";
             }
 			if(content=="Knowledge Center")
             {
